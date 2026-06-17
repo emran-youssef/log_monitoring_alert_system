4d866d1149d481528bf86aac791111670bf46498
Emran Youssef Al-Khaleel <emrankhaleel03@gmail.com>
2026-06-18 01:30:00 +0300

    first commit

---
+++ log-alert-system/src/main/java/com/log_monitoring/log_alert_system/LogAlertSystemApplication.java
@@ -0,0 +1,13 @@
+package com.log_monitoring.log_alert_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAlertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAlertSystemApplication.class, args);
+	}
+
+}
---
+++ log-alert-system/src/test/java/com/log_monitoring/log_alert_system/LogAlertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.log_monitoring.log_alert_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogAlertSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
